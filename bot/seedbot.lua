b73e13da9e76c7d5446ede4c7ca26620a93b1323
exitunlimited <mehdi5874@gmail.com>
2016-09-13 10:52:45 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -230,7 +230,7 @@ function create_config( )
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
+سلام من بات ضد اسپم هستم
 
 https://github.com/SEEDTEAM/TeleSeed
 
